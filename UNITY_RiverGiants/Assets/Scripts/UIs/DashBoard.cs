c1d79a9667aaffd0055be1a44f4b9b79e504c3bb
Eric Lee <leeericyy@gmail.com>
2015-10-29 13:51:52 +0800

    no message

---
+++ UNITY_RiverGiants/Assets/Scripts/UIs/DashBoard.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DashBoard : MonoBehaviour 
+{
+    public Toggle[] toggles;
+    public GameObject[] panels;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+
+    public void ToggleActive()
+    {
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(false);
+        }
+
+        for(int i = 0; i < toggles.Length; i++)
+        {
+            print(toggles[i].isOn);
+            if (toggles[i].isOn == true)
+            {
+                panels[i].SetActive(true);
+                break;
+            }
+        }
+    }
+}
